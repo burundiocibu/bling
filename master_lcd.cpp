8e7e970da4524bbe0e0032620c8856309f5c8721
Jon Little <burundiocibu@gmail.com>
2013-05-14 00:45:30 +0000

    compiles/runs and at least communicates

--- master_lcd.cpp
+++ master_lcd.cpp
@@ -2,6 +2,8 @@
 #include <cstdlib>
 #include <cstdio>
 #include <string>
+#include <vector>
+#include <map>
 #include <iostream>
 #include <iomanip>
 #include <fstream>
@@ -16,17 +18,21 @@
 
 #include "messages.hpp"
 
-RunTime runtime;
-
 void nrf_tx(unsigned slave, uint8_t *buff, size_t len);
 void nrf_rx(unsigned slave);
 void slider(unsigned slave, uint8_t ch, uint16_t &v, int dir);
 void hexdump(uint8_t* buff, size_t len);
-
+void heartbeat(int slave);
 void shutdown(int param);
+void print_slave(int slave);
+void process_ui();
 
 using namespace std;
+
+// Two global objects. sorry
+RunTime runtime;
 ofstream logfile;
+
 
 int main(int argc, char **argv)
 {
@@ -35,7 +41,7 @@ int main(int argc, char **argv)
    signal(SIGTERM, shutdown);
    signal(SIGINT, shutdown);
 
-   if (false)
+   if (true)
    {
       struct sched_param sp;
       memset(&sp, 0, sizeof(sp));
@@ -58,7 +64,7 @@ int main(int argc, char **argv)
    lcd_plate::clear();
    lcd_plate::set_cursor(0,0);
    lcd_plate::set_backlight(lcd_plate::VIOLET);
-   lcd_plate::puts("master_lcd");
+
    logfile << fixed << setprecision(3) << 1e-3*runtime.msec() << hex << endl;
 
    nRF24L01::setup();
@@ -70,62 +76,13 @@ int main(int argc, char **argv)
    nRF24L01::configure_PTX();
    nRF24L01::flush_tx();
 
-   uint8_t buff[::messages::message_size];
-   for (int i=0; i<sizeof(buff); i++) buff[i]=0;
-   uint16_t red=0,green=0,blue=0;
-   unsigned hb_count=0;
-   uint32_t last_hb=0;
-   unsigned slave=0;
-
-   uint8_t button=0;
-   for (int i=0; ; i++)
+   while(true)
    {
-      uint32_t t = runtime.msec();
-
-      if (t - last_hb > 990)
-      {
-         ::messages::encode_heartbeat(buff, t);
-         nrf_tx(slave, buff, sizeof(buff));
-         hb_count++;
-         last_hb = t;
-      }
-
-      uint8_t b = 0x1f & ~lcd_plate::read_buttons();
-      if (b!=button)
-      {
-         button = b;
-         logfile << 1e-3*runtime.msec() << " lcd keypress: " << hex << int(b) << endl;
-         if (b & lcd_plate::LEFT)
-         {
-            slave++;
-            if (slave>3)
-               slave=0;
-            logfile << 1e-3*runtime.msec() << " slave " << slave << endl;
-         }
-         else if (b & lcd_plate::RIGHT)
-         {
-         }
-         else if (b & lcd_plate::UP)
-         {
-            slider(slave, 0, red,   1);
-            slider(slave, 1, green, 1);
-            slider(slave, 2, blue,  1);
-         }
-         else if (b & lcd_plate::DOWN)
-         {
-            slider(slave, 0, red,   -1);
-            slider(slave, 1, green, -1);
-            slider(slave, 2, blue,  -1);
-         }
-         else if (b & lcd_plate::SELECT)
-         {
-            ::messages::encode_start_effect(buff, 0, t, 1000);
-            nrf_tx(slave, buff, sizeof(buff));
-            logfile << 1e-3*runtime.msec() << " effect " << 0 << endl;
-         }
-      }
+      heartbeat(0);
+      process_ui();
       bcm2835_delayMicroseconds(5000);
    }
+
 }
 
 
@@ -168,6 +125,8 @@ void nrf_tx(unsigned slave, uint8_t *buff, size_t len)
       // data doesn't automatically removed...
       flush_tx();
       logfile << 1e-3*runtime.msec() << " ack_err " << ack_err << endl;
+      lcd_plate::set_cursor(0,12);
+      lcd_plate::puts(to_string(ack_err).c_str());
    }
    else if (status & STATUS_TX_DS)
       write_reg(STATUS, STATUS_TX_DS); //Clear the data sent notice
@@ -233,4 +192,136 @@ void slider(unsigned slave, uint8_t ch, uint16_t &v, int dir)
    for (int i=0; i<sizeof(buff); i++) buff[i]=0;
    ::messages::encode_set_tlc_ch(buff, ch, v);
    nrf_tx(slave, buff, sizeof(buff));
+}
+
+void print_slave(int slave)
+{
+   lcd_plate::set_cursor(0,0);
+   lcd_plate::puts(to_string(slave).c_str());
+}
+
+
+void heartbeat(int slave)
+{
+   static uint32_t last_hb=0;
+   if (runtime.msec() - last_hb < 1000)
+      return;
+   uint8_t buff[::messages::message_size];
+   ::messages::encode_heartbeat(buff, runtime.msec());
+   nrf_tx(slave, buff, sizeof(buff));
+   last_hb = runtime.msec();
+}
+
+class Effect
+{
+public:
+   Effect()
+      : state(idle)
+   {}
+
+   Effect(unsigned st, unsigned d, unsigned s) 
+      : start_time(st), duration(d), slave(s),
+        state(pending)
+   {}
+
+   virtual void process(void) = 0;
+   virtual void stop(void) = 0;
+
+   enum State
+   {
+      idle, pending, started
+   } state;
+
+   unsigned start_time;
+   unsigned duration;
+   unsigned slave;
+};
+
+
+class Throb : public Effect
+{
+public:
+   virtual void process(void)
+   {
+      if (state==idle)
+         return;
+
+      uint32_t ms = runtime.msec();
+      if (ms < start_time)
+         return;
+
+      if (ms > start_time + duration)
+      {
+         state=idle;
+         return;
+      }
+
+      state=started;
+      unsigned sec = ms/1000;
+      ms -= sec*1000;
+
+      int v = ms;
+      if (!(sec & 1))
+         v = 1000-ms;
+      uint8_t buff[::messages::message_size];
+      ::messages::encode_set_tlc_ch(buff, 0, v);
+      nrf_tx(slave, buff, sizeof(buff));
+   }
+};
+
+
+void process_ui(void)
+{
+   static unsigned slave=0;
+   static uint16_t red=0,green=0,blue=0;
+   typedef map<string, void *> MenuMap;
+   static MenuMap mm;
+   static MenuMap::const_iterator curr;
+   static uint8_t button=0;
+
+   static bool first_time = true;
+   if (first_time)
+   {
+      lcd_plate::puts("master");
+      print_slave(slave);
+      first_time=false;
+   }
+
+   uint8_t b = 0x1f & ~lcd_plate::read_buttons();
+   if (b!=button)
+   {
+      button = b;
+      logfile << 1e-3*runtime.msec() << " lcd keypress: " << hex << int(b) << endl;
+      if (b & lcd_plate::LEFT)
+      {
+         slave++;
+         if (slave>3)
+            slave=0;
+         print_slave(slave);
+         logfile << 1e-3*runtime.msec() << " slave " << slave << endl;
+      }
+      else if (b & lcd_plate::RIGHT)
+      {
+         //eff.start();
+      }
+      else if (b & lcd_plate::UP)
+      {
+         slider(slave, 0, red,   1);
+         slider(slave, 1, green, 1);
+         slider(slave, 2, blue,  1);
+      }
+      else if (b & lcd_plate::DOWN)
+      {
+         slider(slave, 0, red,   -1);
+         slider(slave, 1, green, -1);
+         slider(slave, 2, blue,  -1);
+      }
+      else if (b & lcd_plate::SELECT)
+      {
+         uint8_t buff[::messages::message_size];
+         ::messages::encode_start_effect(buff, 0, runtime.msec(), 1000);
+         nrf_tx(slave, buff, sizeof(buff));
+         logfile << 1e-3*runtime.msec() << " effect " << 0 << endl;
+      }
+   }
 }
--- rpi_boot.sh
+++ rpi_boot.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
-d=/home/littlej/bling
-logfile=$d/master_lcd.log
-
-bin=master_lcd
-
-echo Start: `date` >> $logfile
-$d/$bin >>$logfile &
+/home/littlej/bling/master_lcd
