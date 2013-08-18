49cc865c88d1ac611bf467caa5060a0cd5ca1db8
abkowitz <abkowitz@twopi.(none)>
2013-08-18 19:05:29 +0000

    added show to make

---
+++ showLists.cpp
@@ -0,0 +1,16 @@
+
+#include <string.h>
+
+#include "showLists.hpp"
+
+namespace showlist
+{
+const int maxNumberEffects = 3;
+ShowEffect showList[maxNumberEffects] = 
+{
+	"Hat Circle",
+	"Sparkles",
+	"Flutes"
+};
+}
+
