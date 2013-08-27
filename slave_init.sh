f87ac9c676eb18dcc558bbd4f7ca981362a61197
Jon Little <burundiocibu@gmail.com>
2013-08-26 20:52:55 -0500

    Finishing out the slave board initialization script

--- slave_init.sh
+++ slave_init.sh
@@ -2,17 +2,30 @@
 
 eval `get_param $1`
 
+loader_hex=../bling-slave/nrf_boot.hex
+show_hex=../bling-slave/slave_main.hex
+
 if [ -z "$slave_addr" ]; then
-    exit
+    exit -1
 fi
 
-avrdude="avrdude -F -p m328p -c usbtiny"
-avrterm="$avrdude -t -q"
+if [ ! -f "$loader_hex" ]; then
+    printf "Could not find $loader_hex"
+    exit -1
+fi
+
+if [ ! -f "$show_hex" ]; then
+    printf "Could not find $show_hex"
+    exit -1
+fi
+
+avrdude="sudo avrdude -F -p m328p -c usbtiny"
+avrterm="$avrdude -t -qq"
 
 echo "Initializing slave ${slave_no}, addr=${slave_addr}"
 
 echo "Writing boot loader to flash ..."
-$avrdude -q -e -U flash:w:nrf_boot.hex:i
+$avrdude -q -e -U flash:w:$loader_hex:i
 
 echo "Writing slave number to eeprom ..."
 echo "write eeprom 0 0" $slave_no | $avrterm
@@ -30,4 +43,16 @@ echo "Writing master's address to eeprom ..."
 echo "write eeprom 12" $master_addr | $avrterm
 
 echo "Dumping eeprom ..."
-echo "dump eeprom 0 16" | $avrterm
+echo "dump eeprom 0 32" | $avrterm
+
+echo "Dumping bootloader ..."
+echo "dump flash 0x7000 32" | $avrterm
+
+echo " "
+
+sudo ./master_loader -s 2 -i $show_hex
+
+echo "Dumping app ..."
+echo "dump flash 0 32" | $avrterm
+
+
