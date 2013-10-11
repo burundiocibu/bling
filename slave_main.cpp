f5e5f8d7a70b096aa0716f6544d97dd825d77054
Jon Little <littlej@arlut.utexas.edu>
2013-10-10 22:47:46 -0500

    Start of a sleep mode

--- slave_main.cpp
+++ slave_main.cpp
@@ -24,6 +24,7 @@ void do_heartbeat(uint8_t* buff, uint32_t& t_hb);
 void do_set_tlc_ch(uint8_t* buff);
 void do_set_rgb(uint8_t* buff);
 void do_ping(uint8_t* buff, uint8_t pipe);
+void do_sleep();
 
 uint16_t slave_id;
 
@@ -87,8 +88,9 @@ int main (void)
             case messages::set_tlc_ch_id:   do_set_tlc_ch(buff); break;
             case messages::set_rgb_id:      do_set_rgb(buff); break;
             case messages::ping_id:         do_ping(buff, pipe); break;
-            case messages::reboot_id:       ((APP*)0)();
-            case boot_id:                   ((APP*)BOOTADDR)(); 
+            case messages::reboot_id:       ((APP*)0)(); break;
+            case boot_id:                   ((APP*)BOOTADDR)(); break;
+            case messages::sleep_id:        do_sleep(); break
          }
       }
       
@@ -180,4 +182,76 @@ void do_ping(uint8_t* buff, uint8_t pipe)
    write_reg(CONFIG, config | CONFIG_PWR_UP); // power back up
    delay_us(150);
    set_CE();
+}
+
+void do_sleep()
+{
+   //avr_tlc5940::setup();
+   // uses TIMER0 for GS clock, TIMER1 to drive blank (and an IRQ)
+   // just disable TIMER0 using PRR
+   PRR |= _BV(PRTIM2);
+
+   // Set the CPU clock prescaler to be 1024. 8MHz / 1024 = 7.8125 kHz
+   CLKPR = CLKPCE;
+   CLKPR = CLKPS3;
+
+   //avr_rtc::setup();
+   // uses TIMER2 to generate a 1kHz interrupt
+   // uses a prescaler of 
+   // lets make this wake us up periodically from sleep
+   // prescaler = 1024, f=7.8125 kHz/1024 = 7.6294 Hz
+   // with OCR=125, 7.6294 / 125 = 0.061035 Hz = 16.38 seconds between IRQ
+   TCCR2B = _BV(CS22|CS21|CS20) ;
+
+   // Turn off 12V supply
+   PORTB &= ~_BV(PB1);
+
+   // Got to "power-save" mode
+   SMCR = SM1 | SM0;   
+   
+   //nRF24L01::setup();
+   // generates an INT0 on rx of data when powered on
+   // leave this as is..
+   using namespace nRF24L01;
+   char config = read_reg(CONFIG);
+
+   while(true)
+   {
+      clear_CE();  // Turn off receiver
+      write_reg(CONFIG, config & ~CONFIG_PWR_UP); // power down
+
+      power_save_mode();
+
+      write_reg(CONFIG, config & CONFIG_PWR_UP); // power up
+      set_CE();
+      
+      power_save_mode();
+      
+      uint8_t status = nRF24L01::read_reg(nRF24L01::STATUS);
+      if (status != 0x0e)
+      {
+         uint8_t pipe;
+         nRF24L01::read_rx_payload(buff, sizeof(buff), pipe);
+         nRF24L01::clear_IRQ();
+
+      }
+   }
+
+
+   // need to turn back on all hardware here...
+
+   // Turn back on the tlc5940 GS clock
+   PRR &= ~_BV(PRTIM2);
+
+   // Restore the prescaler for the tlc5940
+   TCCR0B = _BV(CS00); // prescaler div = 1
+
+   // Restore the prescaler for the rtc
+   TCCR2B = _BV(CS22) ; // prescaler = 64, f=8e6/64=125kHz
+
+   // Turn back on 12V supply
+   PORTB |= _BV(PB1);
+
+   return;
+   // or maybe ((APP*)0)();
 }
