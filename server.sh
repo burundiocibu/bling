4c1a6752c588d5d590c0e9158ab5a73ff999873d
Jon Little <burundiocibu@gmail.com>
2014-09-07 05:08:43 +0000

    New startup script.

--- server.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-base=/home/littlej/dev/bling
-log=/log
-/usr/local/bin/node $base/js/server.js >>$log/server.js.log &
-# 2 lib websockets warnings
-# 8 base output of master_server
-$base/master_server --port 9321 -d $((2+8)) >>$log/master_server.log
