b18c3b5de3c79d3b27655d4865fa0ef91d1817ed
Jon Little <burundiocibu@gmail.com>
2014-09-11 23:21:11 -0500

    Now with a list of effects to walk through.

--- js/www/show.js
+++ js/www/show.js
@@ -1,5 +1,31 @@
 // -*- mode: javascript; -*-
 
+function Event(name, id, duration)
+{
+    this.name = name;
+    this.id = id;  // see Effect.(c|h)pp
+    this.duration = duration;  // ms
+}
+bpm=170; // beats per minute
+mpb = 1000 * 60 / bpm; // ms per beat
+events = [];
+events.push(new Event("set13",   8, 4500));
+events.push(new Event("flash",   1, 4500));
+events.push(new Event("sparkle", 4, 30000));
+events.push(new Event("M3 A1",   9, 5 * mpb));
+events.push(new Event("M3 A2",   9, 5 * mpb));
+
+ep=0;
+
+function update_effect()
+{
+    document.getElementById("play_effect").textContent = ep;
+    document.getElementById("effect_region").value = events[ep].name+
+        " ("+events[ep].id+","+events[ep].duration+")";
+}
+
+update_effect();
+
 new FastButton(document.getElementById('play_effect'), function() { start_effect(); });
 new FastButton(document.getElementById('prev_effect'), function() { prev_effect(); });
 new FastButton(document.getElementById('next_effect'), function() { next_effect(); });
@@ -17,34 +43,36 @@ function all_stop()
 
 function start_effect()
 {
-    var eb = document.getElementById("play_effect");
     var msg = new Start_Effect();
     msg.slave_id = 0; // broadcast
-    msg.effect_id = parseInt(eb.value);
-    eb.value++;
-    eb.textContent=eb.value
+    msg.effect_id = events[ep].id;
     msg.start_time = 0;
-    msg.duration = 10000; // ms
+    msg.duration = events[ep].duration; // ms
     msg.repeat = 10;
     var hdr = new Header();
     hdr.msg_id = "START_EFFECT";
     send_msg(socket, hdr, msg);
+
+    ep++;
+    update_effect();
 }
 
 function prev_effect()
 {
-    var eb = document.getElementById("play_effect");
-    eb.value--;
-    if (eb.value < 1)
+    ep--;
+    if (ep < 0)
     {
-        eb.value = 1;
+        ep = 0;
     }
-    eb.textContent=eb.value;
+    update_effect();
 }
 
 function next_effect()
 {
-    var eb = document.getElementById("play_effect");
-    eb.value++;
-    eb.textContent=eb.value;
+    ep++;
+    if (ep >= events.length)
+    {
+        ep = 0;
+    }
+    update_effect();
 }
